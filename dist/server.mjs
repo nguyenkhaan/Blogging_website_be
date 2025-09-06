f5e153d264bba701b0623774e8deb28e0c76bc83
khaan <nguyenkhaan2006@gmail.com>
2025-09-06 09:45:39 +0700

    them_tsc_alias

--- dist/server.mjs
+++ dist/server.mjs
@@ -1,10 +1,10 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import { router as blogRouter } from './routes/blog.router.mts';
-import { router as userRouter } from './routes/user.router.mts';
-import { router as searchRouter } from './routes/search.router.mts';
-import { jsonConfig, urlEncodedConfig, staticFileConfig } from './config/server.config.mts';
+import { router as blogRouter } from './routes/blog.router';
+import { router as userRouter } from './routes/user.router';
+import { router as searchRouter } from './routes/search.router';
+import { jsonConfig, urlEncodedConfig, staticFileConfig } from './config/server.config';
 dotenv.config({ path: './.env' });
 console.log(process.env.HOST);
 const HOST = process.env.PORT;
